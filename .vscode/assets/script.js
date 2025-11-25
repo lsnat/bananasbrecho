5dcd61b2df8617c4c21bc61a615db7812347048b
Natalia <ronconibnati@gmail.com>
2025-11-24 21:53:46 -0300

    :tada: initial commit

---
+++ .vscode/assets/script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  //if (html.classList.contains('dark')) {
+  // html.classList.remove('dark');
+  // else {
+  // html.classList.add('light');
+
+  html.classList.toggle("dark")
+}
